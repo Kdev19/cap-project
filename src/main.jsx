5656e2245992a2565a2866ddd0f243d9ada769d9
Kdev19 <karlpohjanen99@gmail.com>
2025-01-02 19:32:26 +0100

    Fix to rendering problems

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Nav from './components/Nav'
-import Highlights from './components/Highlights'
-import Testimonials from './components/Testimonials'
-import About from './components/About'
-import Footer from './components/Footer'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import Nav from './components/Nav';
+import Highlights from './components/Highlights';
+import Testimonials from './components/Testimonials';
+import About from './components/About';
+import Footer from './components/Footer';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Nav />
-
-    <Highlights />
-
-    <Testimonials />
-
-    <About />
-
-    <Footer />
-  </StrictMode>,
-)
+    <BrowserRouter> 
+        <Nav />
+        <Highlights />
+        <Testimonials />
+        <About />
+        <Footer />
+    </BrowserRouter>
+  </StrictMode>
+);
