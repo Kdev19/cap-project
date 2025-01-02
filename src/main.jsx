459e77b1c07c9b625ee5f0047942171b3e1d087f
Kdev19 <karlpohjanen99@gmail.com>
2025-01-02 18:47:05 +0100

    Fix to web

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Nav from './components/Nav'
+import Highlights from './components/Highlights'
+import Testimonials from './components/Testimonials'
+import About from './components/About'
+import Footer from './components/Footer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
