22bcc8b4db77f1bf410d70e7f5c6d3e8498290d3
Kdev19 <karlpohjanen99@gmail.com>
2025-01-09 13:35:45 +0100

    HTML markupapplied on bookingForm

--- src/components/Bookingform.jsx
+++ src/components/Bookingform.jsx
@@ -16,51 +16,64 @@ const BookingForm = ({ availableTimes, formData, handleChange, handleSubmit, dis
 
     return (
         <form className="booking-form" onSubmit={handleSubmit}>
-            <label htmlFor="date">Choose a date:</label>
-            <input
-                type="date"
-                id="date"
-                name="date"
-                value={formData.date}
-                onChange={handleDateChange}
-                required
-            />
-            <label htmlFor="time">Choose a time:</label>
-            <select id="time" name="time" value={formData.time} onChange={handleChange} required>
-                {availableTimes && availableTimes.map((time, index) => (
-                    <option key={index} value={time}>{time}</option>
-                ))}
-            </select>
-            <label htmlFor="guests">Number of guests</label>
-            <input
-                type="number"
-                id="guests"
-                name="guests"
-                placeholder="1"
-                min="1"
-                max="10"
-                value={formData.guests}
-                onChange={handleChange}
-                required
-            />
-            <label htmlFor="occasion">Occasion</label>
-            <select
-                id="occasion"
-                name="occasion"
-                value={formData.occasion}
-                onChange={handleChange}
-                required
-            >
-                <option value="">Select occasion</option>
-                <option value="birthday">Birthday</option>
-                <option value="anniversary">Anniversary</option>
-            </select>
-            <input
-                type="submit"
-                value="Make Your reservation"
-                disabled={!isFormValid}
-                className={isFormValid ? 'enabled' : 'disabled'}
-            />
+            <fieldset>
+                <legend>Booking Details</legend>
+                <div className="form-group">
+                    <label htmlFor="date">Choose a date:</label>
+                    <input
+                        type="date"
+                        id="date"
+                        name="date"
+                        value={formData.date}
+                        onChange={handleDateChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="time">Choose a time:</label>
+                    <select id="time" name="time" value={formData.time} onChange={handleChange} required>
+                        {availableTimes && availableTimes.map((time, index) => (
+                            <option key={index} value={time}>{time}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="guests">Number of guests</label>
+                    <input
+                        type="number"
+                        id="guests"
+                        name="guests"
+                        placeholder="1"
+                        min="1"
+                        max="10"
+                        value={formData.guests}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="occasion">Occasion</label>
+                    <select
+                        id="occasion"
+                        name="occasion"
+                        value={formData.occasion}
+                        onChange={handleChange}
+                        required
+                    >
+                        <option value="">Select occasion</option>
+                        <option value="birthday">Birthday</option>
+                        <option value="anniversary">Anniversary</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <input
+                        type="submit"
+                        value="Make Your reservation"
+                        disabled={!isFormValid}
+                        className={isFormValid ? 'enabled' : 'disabled'}
+                    />
+                </div>
+            </fieldset>
         </form>
     );
 };
